49566301231cf0a60f968ad95f9b2224b479bc90
DemirisDev <demiris.costa1229@gmail.com>
2024-09-10 04:03:54 -0700

    update: migrate from cielo to moralis

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
 require('dotenv').config();
-const http = require('http');
-
-// Dummy server to bind to a port
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Bot is running\n');
-});
-
-// Listen on the port provided by Render, or default to 3000
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Bot is running on port ${PORT}`);
-});
-
 const TelegramBot = require('node-telegram-bot-api');
 const { ethers } = require('ethers');
-const axios = require('axios');
+const Moralis = require('moralis').default;
+const { EvmChain } = require('@moralisweb3/common-evm-utils');
 
 // Initialize the bot and provider
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`);
 
-const ERC20_ABI = [
-    "function balanceOf(address account) external view returns (uint256)"
-];
-
 let walletList = {};
-let userStates = {}; // To track user states (for adding or editing wallets)
+let userStates = {};
 
 // Start command that will show the wallet list with action buttons
 bot.onText(/\/start/, async (msg) => {
@@ -48,6 +31,8 @@ bot.onText(/\/start/, async (msg) => {
         }
     });
     showWalletList(chatId);
+
+    await Moralis.start({ apiKey: process.env.MORALIS_API_KEY });
     monitorTransactions(chatId);
 });
 
@@ -61,8 +46,8 @@ async function showWalletList(chatId) {
     for (const [index, wallet] of walletList[chatId].entries()) {
         let walletButtons = [];
         walletButtons.push([
-            { text: 'Edit', callback_data: `edit_${index}` },   // Edit button
-            { text: 'Remove', callback_data: `remove_${index}` }  // Remove button
+            { text: 'Edit', callback_data: `edit_${index}` },
+            { text: 'Remove', callback_data: `remove_${index}` }
         ]);
 
         const options = {
@@ -80,65 +65,56 @@ function monitorTransactions(chatId) {
     let lastTimeStamp = 0;
     // Listen for new blocks
     provider.on('block', async (blockNumber) => {
-        // Get the block details
         const block = await provider.getBlockWithTransactions(blockNumber);
 
-        if(block.timestamp > lastTimeStamp) {
+        if (block.timestamp > lastTimeStamp) {
             lastTimeStamp = block.timestamp;    //Prevent recapturing the same block
 
-            const toAddresses = new Set(block.transactions
-                .filter(tx => tx?.to)
-                .map(tx => tx.to.toLowerCase())
+            const fromAddresses = new Set(block.transactions
+                .map(tx => tx.from.toLowerCase())
             );
 
-            for(const wallet of walletList[chatId].filter(fwallet => toAddresses.has(fwallet.toLowerCase()))) {
-                const options = {
-                    method: 'GET',
-                    url: `https://feed-api.cielo.finance/api/v1/feed?wallet=${wallet}&chains=ethereum&txTypes=transfer&fromTimestamp=${lastTimeStamp}&toTimestamp=${lastTimeStamp}&includeMarketCap=true`,
-                    headers: {
-                        accept: 'application/json',
-                        'X-API-KEY': process.env.CIELO_API_KEY
-                    }
-                };
-                
+            for (const wallet of walletList[chatId].filter(fwallet => fromAddresses.has(fwallet.toLowerCase()))) {
                 try {
-                    const response = await axios.request(options);
+                    const txHistory = await Moralis.EvmApi.wallets.getWalletHistory({
+                        address: wallet,
+                        chain: EvmChain.ETHEREUM,
+                        fromBlock: blockNumber,
+                        toBlock: blockNumber
+                    });
+                    const swapHistory = txHistory.result.filter(h => h.category === 'token swap');
 
-                    if (!response.data.data.items.length) {
-                        continue;
-                    }
-
-                    for (const item of response.data.data.items.filter(fitem => fitem.type === 'ERC20')) {
-                        const tokenContract = new ethers.Contract(item.contract_address, ERC20_ABI, provider);
-                        const balance = await tokenContract.balanceOf(wallet);
-
-                        await bot.sendMessage(
-                            chatId,
-                            "**New Purchase!**\n" +
-                            "**Wallet**:\n`" + wallet + "`\n" +
-                            "**Token Address**:\n`" + item.contract_address + "`\n" +
-                            "**Market Cap**:\n" + item.token_market_cap.market_cap + "\n" +
-                            "**Position**:\n" + ethers.utils.formatUnits(balance, 18) + "\n" +
-                            "**Tx**:\n" + 
-                                "- Hash: " + item.tx_hash + "\n" +
-                                "- Token Name: " + item.symbol + "\n" +
-                                "- Token Price: " + item.token_price_usd + " (USD)\n" +
-                                "- Amount: " + item.amount_usd + " (USD)\n" +
-                            "**Entry Time**:\n" + lastTimeStamp + "\n" +
-                            "[Token Icon](" + item.token_icon_link + ")",
-                            {
-                                parse_mode: 'Markdown',
-                                reply_markup: {
-                                    inline_keyboard: [[
-                                        { text: 'Buy on Banana Gun', url: `https://bananagun.com/${item.contract_address}` },
-                                        { text: 'Open on Dexscreener', url: `https://dexscreener.com/ethereum/${item.contract_address}` }
-                                    ]]
+                    for (const swap of swapHistory) {
+                        for (const item of swap.erc20Transfers) {
+                            console.log(item)
+                            const tokenAddress = item.address.lowercase;
+                            const tokenName = item.tokenName;
+                            const tokenSymbol = item.tokenSymbol;
+                            const swapType = item.fromAddress.equals(wallet) ? 'Sell' : 'Buy';
+                            const amount = item.valueFormatted;
+                            await bot.sendMessage(
+                                chatId,
+                                "New Transaction - " + swapType + "!\n" +
+                                "Wallet:\n`" + wallet + "`\n" +
+                                "Token Address:\n`" + tokenAddress + "`\n" +
+                                "Token Name: " + tokenName + "\n" +
+                                "Token Symbol: " + tokenSymbol + "\n" +
+                                "Amount: " + amount + "\n" +
+                                "Entry Time: " + lastTimeStamp + "\n",
+                                {
+                                    parse_mode: 'Markdown',
+                                    reply_markup: {
+                                        inline_keyboard: [[
+                                            { text: 'Buy on Banana Gun', url: `https://t.me/BananaGunSniper_bot` },
+                                            { text: 'Open on Dexscreener', url: `https://dexscreener.com/ethereum/${tokenAddress}` }
+                                        ]]
+                                    }
                                 }
-                            }
-                        );
+                            );
+                        }
                     }
                 } catch (error) {
-                    console.error('Error fetching wallet data:', error.message);
+                    console.error(error);
                 }
             }
         }
@@ -194,7 +170,7 @@ bot.on('message', async (msg) => {
             userStates[chatId] = null;
             return;
         }
-        
+
         walletList[chatId][index] = walletAddress;
         userStates[chatId] = null;
         showWalletList(chatId);
@@ -205,7 +181,7 @@ bot.on('message', async (msg) => {
             return;
         }
 
-        if(walletList[chatId].includes(walletAddress)) {
+        if (walletList[chatId].includes(walletAddress)) {
             await bot.sendMessage(chatId, `The address exists in the list.`);
             userStates[chatId] = null;
             return;
